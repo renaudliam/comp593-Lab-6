d3a45db606a2cb9c6a367a7dbb509b32add9c17a
renaudliam <liam.renaud@flemingcollege.ca>
2022-03-27 21:56:28 -0400

    Question 1 copy

--- scratch_20.py
+++ scratch_20.py
@@ -1,11 +1,33 @@
 import requests
-def request():
-    response = requests.head("https://pokeapi.co/api/v2/")
 
+
+def main():
+
+
+    poke_info = get_poke_info()
+
+    if poke_info:
+
+        print("Weight:", poke_info['weight'])
+        print("Ability 1:", poke_info["abilities"][0]['ability']["name"])
+        print("Ability 2:", poke_info['abilities'][1]['ability']["name"])
+        print("Height:", poke_info['height'])
+
+
+
+
+
+def get_poke_info():
+    pokemon = input("Enter a Pokemon")
+    print("Getting pokemon information..." " ", end="")
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon))
 
     if response.status_code == 200:
-        print('Response:', response.status_code, "Connection Successful!" '\n')
-    else:
-        print('Uh Oh, got', response.status_code)
+        print("Success! You can find the details below!")
+        return response.json()
 
-    print(response.headers)
+    else:
+        print('Connection failed...', response.status_code)
+        return
+
+main()
